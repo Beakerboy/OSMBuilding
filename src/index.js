12f6b0997c87bcf35388c0493ae9907f3b52f180
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-03-24 12:46:00 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import * as THREE from "three";
+import * as THREE from "https://cdn.skypack.dev/three";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 var camera;
 var renderer;
