f3330c4888d29948aba29924971a6b64e64c061a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-03-24 12:53:55 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import * as THREE from "https://cdn.skypack.dev/pin/three@v0.138.1-reCevIfO2moyjiiuoEbR/mode=imports/optimized/three.js";
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from "https://cdn.skypack.dev/pin/three@v0.138.1-reCevIfO2moyjiiuoEbR/mode=raw/examples/jsm/controls/OrbitControls.js";
 var camera;
 var renderer;
 var controls;
