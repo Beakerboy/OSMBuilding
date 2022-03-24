c3a2dd61c665572deb7d038397edc39d9160f09d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-03-24 12:08:31 -0400

    Update App.js

--- App.js
+++ App.js
@@ -24,25 +24,30 @@ function init() {
   renderer.domElement.style.zIndex = 1;
   renderer.domElement.style.top = 0;
   document.body.appendChild(renderer.domElement);
-  var shape = [];
-  var geometry = new THREE.IcosahedronGeometry(2, 0);
+  const shape = new THREE.Shape();
+  shape.moveTo( 0,0 );
+  shape.lineTo( 0, 1 );
+  shape.lineTo( 2, 1 );
+  shape.lineTo( 2, 0 );
+  shape.lineTo( 1, 0 );
+  shape.lineTo( 1, .5 );
+  shape.lineTo( .5, .5 );
+  shape.lineTo( .5, 0 );
+  shape.lineTo( 0, 0 );
+  const extrudeSettings = {
+    steps: 2,
+    depth: 1,
+    bevelEnabled: false,
+  };
+  var geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
   var material = new THREE.MeshLambertMaterial({
     color: 0x0064ff,
     emissive: 0x1111111
   });
-  var geometryFrame = new THREE.IcosahedronGeometry(4, 0);
-  var materialFrame = new THREE.MeshBasicMaterial({
-    wireframe: true,
-    transparent: true,
-    opacity: 0.1,
-    color: 0xffffff
-  });
-  shape[0] = new THREE.Mesh(geometryFrame, materialFrame);
+
+  shape[0] = new THREE.Mesh(geometry, material);
   shape[0].position.set(3, 5, 0);
-  shape[1] = new THREE.Mesh(geometry, material);
-  shape[1].position.set(3, 5, 0);
   scene.add(shape[0]);
-  scene.add(shape[1]);
 
   var pointLight = new THREE.PointLight(0x888888);
   pointLight.position.set(1, 100, 500);
@@ -52,10 +57,7 @@ function init() {
   controls = new OrbitControls( camera, renderer.domElement );
   function render() {
     requestAnimationFrame(render);
-    shape[0].rotation.x -= 0.005;
-    shape[0].rotation.y -= 0.005;
-    shape[1].rotation.x += 0.02;
-    shape[1].rotation.y += 0.02;
+
     renderer.render(scene, camera);
   }
   render();
@@ -75,12 +77,4 @@ function resize() {
     document.documentElement.clientHeight
   );
 
-  function onWindowResize() {
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-  }
 }
