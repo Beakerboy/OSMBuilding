7e1304435d5c7fe7ed082d15ff69506913695607
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-03-29 10:51:45 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -337,7 +337,10 @@ function createRoof(way, xml_data, home_lat, home_lon) {
     const R = calculateWayRadius(way, xml_data);
     const geometry = new THREE.SphereGeometry( R, 100, 100, 0, 2 * Math.PI, Math.PI/2 );
     // Adjust the dome height if needed.
-    geometry.scale.z = (roof_height / R);
+    if (roof_height === 0) {
+      roof_height = R;
+    }
+    geometry.scale(1, 1, roof_height / R);
     const material = new THREE.MeshBasicMaterial( { color: 0xeeeeee } );
     const roof = new THREE.Mesh( geometry, material );
     const elevation = calculateWayHeight(way) - calculateRoofHeight(way);
