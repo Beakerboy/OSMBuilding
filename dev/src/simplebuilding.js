d59a9155db3b9e71d5b0dbe272573779b58b25e9
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-04-02 12:08:04 -0400

    Update simplebuilding.js

--- dev/src/simplebuilding.js
+++ dev/src/simplebuilding.js
@@ -1,1 +1,34 @@
+class SimpleBuilding {
+  constructor(way_id) {
+    this.id = way_id;
+    // get full way data from OSM
+    // get bounding box data from OSM
+    // Transform lat-lon to x-y.
+    // This.nodeList = all nodes
+    // discard nodes not within the main building way.
 
+    // discard any ways that contain missing nodes
+    // or are not building parts.
+  }
+
+  /**
+   * Is this point inside this building?
+   *
+   * This may be better in a 2DShape class to manage
+   * 2D geometry math functions.
+   */
+  surrounds(x, y) {
+
+  }
+
+  /**
+   * Discard any nodes that are not within the building
+   */
+  discardOutsideNodes() {
+    // foreach this.nodeList as node
+    //   if (!this.surrounds(node)) {
+    //     unset (this.nodelist[i]);
+    //   }
+  }
+  
+}
