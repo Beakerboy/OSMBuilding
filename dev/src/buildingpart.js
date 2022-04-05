41b953808a283a61ac39dd00cc423c1ff41d7373
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-04-05 13:04:55 -0400

    Update buildingpart.js

--- dev/src/buildingpart.js
+++ dev/src/buildingpart.js
@@ -4,6 +4,9 @@ class BuildingPart {
   
   constructor(way, building) {
     this.way = way;
+    this.height = calculateWayHeight(way);
+    this.min_height = calculateWayMinHeight(way);
+    this.roof_height = calculateRoofHeight(way);
   }
 
   calculateRadius(xml_data) {
@@ -56,6 +59,26 @@ class BuildingPart {
   }
 
   render() {
-    
+    let extrusion_height = this.height - this.min_height - this.roof_height;
+
+    // If we have a multi-polygon, create the outer shape
+    // then punch out all the inner shapes.
+    var shape = this.createShape();
+    let extrudeSettings = {
+      bevelEnabled: false,
+      depth: extrusion_height
+    };
+    var geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+
+    // Create the mesh.
+    // Todo: Use an array of materials to render the roof the appropriate color.
+    var mesh = new THREE.Mesh(geometry, [getRoofMaterial(way), getMaterial(way)]);
+
+    // Change the position to compensate for the min_height
+    mesh.rotation.x = -Math.PI / 2;
+    mesh.position.set( 0, min_height, 0);
+    scene.add( mesh );
+
+    this.createRoof();
   }
 }
