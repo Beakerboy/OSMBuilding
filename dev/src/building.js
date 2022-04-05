93ecf93cb21837404081c00dbe2405df8c040a1e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-04-05 10:11:28 -0400

    Update building.js

--- dev/src/building.js
+++ dev/src/building.js
@@ -7,6 +7,7 @@ class Building {
   outer;
 
   id = 0;
+  nodelist;
   static async create(id) {
     const data = await Building.getData(id);
     return new Building(data);
@@ -16,68 +17,64 @@ class Building {
     let xml_data = new window.DOMParser().parseFromString(data, "text/xml");
     
     if (Building.isValidData(xml_data)) {
-        const node_list = xml_data.getElementsByTagName("node");
-        // convert the node_list to a associative array
-        const way_nodes = xml_data.getElementsByTagName("nd");
-        // if it is a building, query all ways within the bounding box and reder the building parts.
-        // The way is a list of <nd ref=""> tags.
-        // Use the ref to look up the lat/log data from the unordered <node id="" lat="" lon=""> tags.
-        var lats = [];
-        var lons = [];
-        var lat = 0;
-        var lon = 0;
-        var node;
-        var ref;
-        for (let i = 0; i < way_nodes.length; i++) {
-          ref = way_nodes[i].getAttribute("ref");
-          node = xml_data.querySelector('[id="' + ref + '"]');
-          lat = node.getAttribute("lat");
-          lon = node.getAttribute("lon");
-          lats.push(lat);
-          lons.push(lon);
-        }
-        // Get all building parts within the building
-        // Get max and min lat and log from the building
-        const left = Math.min(...lons);
-        const bottom = Math.min(...lats);
-        const right = Math.max(...lons);
-        const top = Math.max(...lats);
+      const way_nodes = xml_data.getElementsByTagName("nd");
+      this.outer = xml_data.getElementsByTagName("way");
+      // if it is a building, query all ways within the bounding box and reder the building parts.
+      // The way is a list of <nd ref=""> tags.
+      // Use the ref to look up the lat/log data from the unordered <node id="" lat="" lon=""> tags.
+      var lats = [];
+      var lons = [];
+      var lat = 0;
+      var lon = 0;
+      var node;
+      var ref;
+      for (let i = 0; i < way_nodes.length; i++) {
+        ref = way_nodes[i].getAttribute("ref");
+        node = xml_data.querySelector('[id="' + ref + '"]');
+        lat = node.getAttribute("lat");
+        lon = node.getAttribute("lon");
+        lats.push(lat);
+        lons.push(lon);
+      }
+      // Get all building parts within the building
+      // Get max and min lat and log from the building
+      const left = Math.min(...lons);
+      const bottom = Math.min(...lats);
+      const right = Math.max(...lons);
+      const top = Math.max(...lats);
 
-        // Set the "home point", the lat lon to center the structure.
-        const home_lon = (left + right) / 2;
-        const home_lat = (top + bottom) / 2;
-        this.home = [home_lat, home_lon];
+      // Set the "home point", the lat lon to center the structure.
+      const home_lon = (left + right) / 2;
+      const home_lat = (top + bottom) / 2;
+      this.home = [home_lat, home_lon];
   
-        const helper_size = Math.max(right - left, top - bottom) * 2 * Math.PI * 6371000  / 360 / 0.9;
-        const helper = new THREE.GridHelper(helper_size, helper_size / 10);
-        scene.add(helper);
+      const helper_size = Math.max(right - left, top - bottom) * 2 * Math.PI * 6371000  / 360 / 0.9;
+      const helper = new THREE.GridHelper(helper_size, helper_size / 10);
+      scene.add(helper);
 
-        this.addParts(left, bottom, right, top);
+      this.addParts(left, bottom, right, top);
   
-       // Add the main building if no parts were rendered.
-        if (k === 0) {
-          var shape = this.createShape(xml_data, xml_data);
-          extrudeSettings = {
-            bevelEnabled: false,
-            depth: calculateWayHeight(xml_data)
-          };
-          var geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
-          const building_mesh = new THREE.Mesh(geometry, material);
-          building_mesh.rotation.x = -Math.PI / 2;
-          scene.add(building_mesh);
-        }
-        // get full way data from OSM
-        // get bounding box data from OSM
-        // Transform lat-lon to x-y.
-        // This.nodeList = all nodes
-        // discard nodes not within the main building way.
-
-        // ways = get all ways.
-        // foreach ways as way
-        //   discard any ways that contain missing nodes
-        //   or are not building parts.
+      this.render();
     } else {
       console.log("XML Not Valid")
+    }
+  }
+
+  render() {
+    if (this.parts.length > 0) {
+       for (let i = 0; i < this.parts.length; i++) {
+         this.renderPart(this.parts[i]);
+       }
+    } else {
+      var shape = this.createShape(xml_data);
+      extrudeSettings = {
+        bevelEnabled: false,
+        depth: calculateWayHeight(xml_data)
+      };
+      var geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+      const building_mesh = new THREE.Mesh(geometry, material);
+      building_mesh.rotation.x = -Math.PI / 2;
+      scene.add(building_mesh);
     }
   }
 
@@ -85,6 +82,7 @@ class Building {
     // Get all objects in that area.
     let innerData = await Building.getInnerData(left, bottom, right, top);
     let inner_xml_data = new window.DOMParser().parseFromString(innerData, "text/xml");
+    this.nodelist = inner_xml_data;
 
     // Filter to all ways
     const innerWays = inner_xml_data.getElementsByTagName("way");
@@ -96,33 +94,36 @@ class Building {
     var extrusion_height = 0;
     for (let j = 0; j < innerWays.length; j++) {
       if (innerWays[j].querySelector('[k="building:part"]')) {
-        height = calculateWayHeight(innerWays[j]);
-        min_height = calculateWayMinHeight(innerWays[j]);
-        roof_height = calculateRoofHeight(innerWays[j]);
-        extrusion_height = height - min_height - roof_height;
-
-        // If we have a multi-polygon, create the outer shape
-        // then punch out all the inner shapes.
-        var shape = this.createShape(innerWays[j], inner_xml_data);
-        k++;
-        extrudeSettings = {
-          bevelEnabled: false,
-          depth: extrusion_height
-        };
-        var geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
-
-        // Create the mesh.
-        // Todo: Use an array of materials to render the roof the appropriate color.
-        var mesh = new THREE.Mesh(geometry, [getRoofMaterial(innerWays[j]), getMaterial(innerWays[j])]);
-
-        // Change the position to compensate for the min_height
-        mesh.rotation.x = -Math.PI / 2;
-        mesh.position.set( 0, min_height, 0);
-        scene.add( mesh );
-
-        this.createRoof(innerWays[j], inner_xml_data);
+        this.parts.push(innerWays[j]);
       }
     }
+  }
+
+  renderPart(way) {
+    let height = calculateWayHeight(way);
+    let min_height = calculateWayMinHeight(way);
+    let roof_height = calculateRoofHeight(way);
+    let extrusion_height = height - min_height - roof_height;
+
+    // If we have a multi-polygon, create the outer shape
+    // then punch out all the inner shapes.
+    var shape = this.createShape(way);
+    let extrudeSettings = {
+      bevelEnabled: false,
+      depth: extrusion_height
+    };
+    var geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+
+    // Create the mesh.
+    // Todo: Use an array of materials to render the roof the appropriate color.
+    var mesh = new THREE.Mesh(geometry, [getRoofMaterial(innerWays[j]), getMaterial(innerWays[j])]);
+
+    // Change the position to compensate for the min_height
+    mesh.rotation.x = -Math.PI / 2;
+    mesh.position.set( 0, min_height, 0);
+    scene.add( mesh );
+
+    this.createRoof(way, this.nodelist);
   }
 
   /**
@@ -179,14 +180,14 @@ class Building {
    * way DOM tree of the way to render
    * xml_data the DOM tree of all the data in the region
    */
-   createShape(way, xml_data) {
+   createShape(way) {
     const elements = way.getElementsByTagName("nd");
     const shape = new THREE.Shape();
     var lat = 0;
     var lon = 0;
     for (let i = 0; i < elements.length; i++) {
       var ref = elements[i].getAttribute("ref");
-      var node = xml_data.querySelector('[id="' + ref + '"]');
+      var node = this.nodelist.querySelector('[id="' + ref + '"]');
       lat = parseFloat(node.getAttribute("lat"));
       lon = parseFloat(node.getAttribute("lon"));
       var points = this.repositionPoint([lat, lon]);
