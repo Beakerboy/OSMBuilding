21c502d8b2165184482c505f83305ce71907e02a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-04-08 12:22:54 -0400

    Update PyramidGeometry.js

--- src/geometries/PyramidGeometry.js
+++ src/geometries/PyramidGeometry.js
@@ -14,13 +14,56 @@ class PyramidGeometry extends THREE.BufferGeometry {
       options: options
     };
 
-    const scope = this;
-
     const verticesArray = [];
     const indices = [];
     const uvArray = [];
 
-    addShape( shape );
+    // options
+    const curveSegments = options.curveSegments !== undefined ? options.curveSegments : 12;
+    let depth = options.depth !== undefined ? options.depth : 1;
+    let center = options.center !== undefined ? options.center : [0, 0];
+
+    // Variables initialization
+    const shapePoints = shape.extractPoints( curveSegments );
+
+    // vertices is an array of 2D Vectors.
+    // Does it duplicate the strat/end point?
+    let vertices = shapePoints.shape;
+    console.log(vertices);
+    console.log("end");
+    let groupStart = 0;
+    const reverse = ! THREE.ShapeUtils.isClockWise( vertices );
+    if ( reverse ) {
+      vertices = vertices.reverse();
+    }
+
+    for (let i=0; i < vertices.length / 2; i++) {
+      verticesArray.push(vertices[i].x, vertices[i].y, 0);
+    }
+
+    // An Array of Indices i.e. [[3,2,0], [2,1,0]] 
+    const faces = THREE.ShapeUtils.triangulateShape(vertices, []);
+    for (let k = 0; k < faces.length; k++) {
+      indices.push(...faces[k]);
+    }
+
+    // add a group for the base
+    this.addGroup(groupStart, verticesArray.length / 3, 0);
+    groupStart += verticesArray.length / 3;
+
+    // Add the center point to the list of vertices.
+    vertices.push(...center, depth);
+
+    // create the index list for the sides
+    // basePoints is the index of the center point as well.
+    const basePoints = vertices.length;
+    for (let j = 0; j < basePoints - 1; j++) {
+      indices.push(j, basePoints, j + 1);
+    }
+    // Add the final triangle to connect the first and last point.
+    indices.push(basePoints - 1, basePoints, 0);
+    // add a group for the side.
+    scope.addGroup(groupStart, basePoints, 1);
 
     // build geometry
 
@@ -28,54 +71,5 @@ class PyramidGeometry extends THREE.BufferGeometry {
     this.setAttribute( 'position', new THREE.BufferAttribute(new Float32Array(verticesArray), 3));
 
     this.computeVertexNormals();
-
-    function addShape( shape ) {
-      const verticesArray = [];
-
-      // options
-      const curveSegments = options.curveSegments !== undefined ? options.curveSegments : 12;
-      let depth = options.depth !== undefined ? options.depth : 1;
-      let center = options.center !== undefined ? options.center : [0, 0];
-
-      // Variables initialization
-      const shapePoints = shape.extractPoints( curveSegments );
-
-      // vertices is an array of 2D Vectors.
-      // Does it duplicate the strat/end point?
-      let vertices = shapePoints.shape;
-      let groupStart = 0;
-      const reverse = ! THREE.ShapeUtils.isClockWise( vertices );
-      if ( reverse ) {
-        vertices = vertices.reverse();
-      }
-
-      for (let i=0; i < vertices.length / 2; i++) {
-          verticesArray.push(vertices[i].x, vertices[i].y, 0);
-      }
-
-      // An Array of Indices i.e. [[3,2,0], [2,1,0]] 
-      const faces = THREE.ShapeUtils.triangulateShape(vertices, []);
-      for (let k = 0; k < faces.length; k++) {
-        indices.push(...faces[k]);
-      }
-     
-      // add a group for the base
-      scope.addGroup(groupStart, verticesArray.length / 3, 0);
-      groupStart += verticesArray.length / 3;
-
-      // Add the center point to the list of vertices.
-      vertices.push(...center, depth);
-
-      // create the index list for the sides
-      // basePoints is the index of the center point as well.
-      const basePoints = vertices.length;
-      for (let j = 0; j < basePoints - 1; j++) {
-        indices.push(j, basePoints, j + 1);
-      }
-      // Add the final triangle to connect the first and last point.
-      indices.push(basePoints - 1, basePoints, 0);
-      // add a group for the side.
-      scope.addGroup(groupStart, basePoints, 1);
-    }
   }
 }
