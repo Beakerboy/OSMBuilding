0d8b03f66218d638cf50a294fcaa99ff56a47bec
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-04-12 08:54:39 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,29 +6,6 @@ var home;
 var helper_size;
 var building = {};
 
-  let apis = {
-    bounding: {
-      api:"https://api.openstreetmap.org/api/0.6/map?bbox=",
-      url: (left, bottom, right, top) => {
-        return apis.bounding.api + left + "," + bottom + "," + right + "," + top;
-      }
-    },
-    get_relation: {
-      api:"https://api.openstreetmap.org/api/0.6/relation/",
-      parameters:"/full",
-      url: (relation_id) => {
-        return apis.get_relation.api + relation_id + apis.get_relation.parameters;
-      }
-    },
-    get_way: {
-      api:"https://api.openstreetmap.org/api/0.6/way/",
-      parameters:"/full",
-      url: (way_id) => {
-        return apis.get_way.api + way_id + apis.get_way.parameters;
-      }
-    }
-  };
-
 /**
  * Initialize the screen
  */
