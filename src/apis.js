76eea348dcf5431d0e1827b7aae1fe353dfcfad9
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-04-13 09:20:17 -0400

    Single quote

--- src/apis.js
+++ src/apis.js
@@ -1,20 +1,20 @@
 let apis = {
   bounding: {
-    api:"https://api.openstreetmap.org/api/0.6/map?bbox=",
+    api:'https://api.openstreetmap.org/api/0.6/map?bbox=',
     url: (left, bottom, right, top) => {
-      return apis.bounding.api + left + "," + bottom + "," + right + "," + top;
+      return apis.bounding.api + left + ',' + bottom + ',' + right + ',' + top;
     }
   },
   get_relation: {
-    api:"https://api.openstreetmap.org/api/0.6/relation/",
-    parameters:"/full",
+    api:'https://api.openstreetmap.org/api/0.6/relation/',
+    parameters:'/full',
     url: (relation_id) => {
       return apis.get_relation.api + relation_id + apis.get_relation.parameters;
     }
   },
   get_way: {
-    api:"https://api.openstreetmap.org/api/0.6/way/",
-    parameters:"/full",
+    api:'https://api.openstreetmap.org/api/0.6/way/',
+    parameters:'/full',
     url: (way_id) => {
       return apis.get_way.api + way_id + apis.get_way.parameters;
     }
