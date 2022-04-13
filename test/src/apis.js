c8519f42752fa57c98b9530a4fc8b24abec7426d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-04-12 21:21:49 -0400

    Update apis.js

--- test/src/apis.js
+++ test/src/apis.js
@@ -1,18 +1,18 @@
 let apis = {
   bounding: {
-    api:"https://beakerboy.github.io/OSMBuilding/test/fetch.html?type=map&bbox=",
+    api:"https://beakerboy.github.io/OSMBuilding/test/map/",
     url: (left, bottom, right, top) => {
       return apis.bounding.api + left + "," + bottom + "," + right + "," + top;
     }
   },
   get_relation: {
-    api:"https://beakerboy.github.io/OSMBuilding/test/fetch.html?type=relation&id=",
+    api:"https://beakerboy.github.io/OSMBuilding/test/relation/",
     url: (relation_id) => {
       return apis.get_relation.api + relation_id;
     }
   },
   get_way: {
-    api:"https://beakerboy.github.io/OSMBuilding/test/fetch.html?type=way&id=",
+    api:"https://beakerboy.github.io/OSMBuilding/test/way/",
     url: (way_id) => {
       return apis.get_way.api + way_id;
     }
