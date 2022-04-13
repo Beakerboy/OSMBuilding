17031215980e5a49dace463e2b7db7223fc7b3a4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-04-13 10:12:28 -0400

    Ws

--- src/index.js
+++ src/index.js
@@ -78,24 +78,7 @@ function addLights() {
   var dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
   dirLight.position.set( -1, 0.75, 1 );
   dirLight.position.multiplyScalar( 1000 );
-  //dirLight.name = "dirlight";
-   // dirLight.shadowCameraVisible = true;
-
-   scene.add( dirLight );
-
-   //dirLight.castShadow = true;
-   //dirLight.shadowMapWidth = dirLight.shadowMapHeight = 1024*2;
-
-   //var d = 300;
-
-   //dirLight.shadowCameraLeft = -d;
-   //dirLight.shadowCameraRight = d;
-   //dirLight.shadowCameraTop = d;
-   //dirLight.shadowCameraBottom = -d;
-
-   //dirLight.shadowCameraFar = 3500;
-   //dirLight.shadowBias = -0.0001;
-   //dirLight.shadowDarkness = 0.35;
+  scene.add( dirLight );
 }
 
 init();
