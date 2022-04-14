fda241397616d86e3d72229550526c718b5b82ba
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-04-14 12:58:20 -0400

    Update apis.js

--- src/apis.js
+++ src/apis.js
@@ -8,15 +8,15 @@ let apis = {
   getRelation: {
     api:'https://api.openstreetmap.org/api/0.6/relation/',
     parameters:'/full',
-    url: (relation_id) => {
-      return apis.getRelation.api + relation_id + apis.getRelation.parameters;
+    url: (relationId) => {
+      return apis.getRelation.api + relationId + apis.getRelation.parameters;
     }
   },
   getWay: {
     api:'https://api.openstreetmap.org/api/0.6/way/',
     parameters:'/full',
-    url: (way_id) => {
-      return apis.get_way.api + way_id + apis.getWay.parameters;
+    url: (wayId) => {
+      return apis.getWay.api + wayId + apis.getWay.parameters;
     }
   }
 };
