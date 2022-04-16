85eb8ccc84065740666a3507a29637b9a46177fc
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-04-16 07:27:09 -0400

    Update multibuildingpart.js

--- src/multibuildingpart.js
+++ src/multibuildingpart.js
@@ -9,10 +9,10 @@ class MultiBuildingPart extends BuildingPart {
     this.way = fullXmlData.getElementById(id);
     const inner_members = this.way.querySelectorAll('member[role="inner"]');
     const outer_members = this.way.querySelectorAll('member[role="outer"]');
-    const this.inner_shapes = [];
+    const inner_shapes = [];
     for (let i = 0; i < inner_members.length; i++) {
       const way = fullXmlData.getElementById(inner_members[i].getAttribute('ref'));
-      this.inner_shapes.push(BuildingShapeUtils.createSHape(way, nodelist));
+      inner_shapes.push(BuildingShapeUtils.createShape(way, nodelist));
     }
     for (let j = 0; j < inner_members.length; j++) {
       const way = fullXmlData.getElementById(outer_members[j].getAttribute('ref'));
