cb5c722a3b2e853fdbfafd7e7b345a6b13397c4c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-04-18 11:46:00 -0400

    Update multibuildingpart.js

--- src/multibuildingpart.js
+++ src/multibuildingpart.js
@@ -7,12 +7,10 @@ class MultiBuildingPart extends BuildingPart {
 
   buildShape() {
     this.type = 'multipolygon';
-    console.log('running multiBuilidngPart.buildshape');
     const innerMembers = this.way.querySelectorAll('member[role="inner"]');
     const outerMembers = this.way.querySelectorAll('member[role="outer"]');
     const innerShapes = [];
     var shapes = [];
-    console.log('Multipolygon ' + this.id + ' has ' + outerMembers.length + ' outer and ' + innerMembers.length + ' inner members');
     for (let i = 0; i < innerMembers.length; i++) {
       const way = this.fullXmlData.getElementById(innerMembers[i].getAttribute('ref'));
       innerShapes.push(BuildingShapeUtils.createShape(way, this.nodelist));
@@ -29,7 +27,6 @@ class MultiBuildingPart extends BuildingPart {
   getWidth() {
     var xy = [[], []];
     for (let i = 0; i < this.shape.length; i++){
-      console.log(i);
       const shape = this.shape[i];
       const newXy = BuildingShapeUtils.combineCoordinates(shape);
       xy[0] = xy[0].concat(newXy[0]);
