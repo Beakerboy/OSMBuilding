9bf284c18773c65bd1485914351986a52be8a27e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-04-18 10:59:12 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ function init() {
       });
   }
   Building.create(type, id).then(function(myObj){
-    const helper_size = myObj.outer_element.getWidth();
+    const helper_size = myObj.outerElement.getWidth();
     const helper = new THREE.GridHelper(helper_size / 0.9, helper_size / 9);
     scene.add(helper);
 
