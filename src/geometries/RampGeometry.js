1dd48c7cde8abacf9a3988a9bd4344340b5f1c70
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-04-19 17:52:12 -0400

    Update RampGeometry.js

--- src/geometries/RampGeometry.js
+++ src/geometries/RampGeometry.js
@@ -1,3 +1,28 @@
+/**
+ * Modify ExtrudeGeometry such that z varies with x and y
+ */
 class RampGeometry extends THREE.BufferGeometry () {
+  constructor(shape = new Shape( [ new Vector2( 0.5, 0.5 ), new Vector2( - 0.5, 0.5 ), new Vector2( - 0.5, - 0.5 ), new Vector2( 0.5, - 0.5 ) ] ), options = {}) {
+    super();
 
+    this.type = 'RampGeometry';
+
+    this.parameters = {
+      shape: shape,
+      options: options,
+    };
+    const depth = options.depth;
+    const angle = options.angle;
+    const m = new THREE.Matrix3();
+    m.set([Math.cos(angle), Math.sin(angle), 0,
+      -Math.sin(angle), Math.cos(angle), 0,
+      Math.sin(angle), -Math.cos(angle), 0]);
+
+    var points = shape.extractPoints().shape;
+    const reverse = ! THREE.ShapeUtils.isClockWise( points );
+    if ( reverse ) {
+      points = points.reverse();
+    }
+
+  }
 }
