b95f45971ebffd8cf4b75a3d063fb6455e4ffff3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-04-19 22:11:29 -0400

    Update RampGeometry.js

--- src/geometries/RampGeometry.js
+++ src/geometries/RampGeometry.js
@@ -23,6 +23,31 @@ class RampGeometry extends THREE.BufferGeometry () {
     if ( reverse ) {
       points = points.reverse();
     }
+    var rampDepth;
+    var minDepth;
+    var maxDepth;
+    for (let i = 0; i < points.length - 1; i++) {
+      point = points[i];
+      nextPoint = points[i + 1];
+      positions.push(point.x, point.y, 0);
+      rampDepth = point.x * Math.sin(angle) - point.y * Math.cos(angle);
+      if (i === 0) {
+        minDepth = rampDepth;
+        maxDepth = rampDepth;
+      } else {
+        minDepth = Math.min(rampDepth, minDepth);
+        maxDepth = Math.max(rampDepth, maxDepth);
+      }
+      positions.push(point.x, point.y, rampDepth);
+      positions.push(nextPoint.x, nextPoint.y, 0);
+      positions.push(point.x, point.y, rampDepth);
+      positions.push(nextPoint.x, nextPoint.y, rampDepth);
+      positions.push(nextPoint.x, nextPoint.y, 0);
+    }
+    
+    this.setAttribute('position', new THREE.BufferAttribute(new Float32Array(positions), 3));
+    // ToDo - add points correctly so only one face needs to be rendered.
+    this.computeVertexNormals();
 
   }
 }
