1584f5a6466acf6cab152496b010632834fbb4cd
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-04-21 15:26:02 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+import {
+  GridHelper,
+  OrbitControls,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+  AmbientLight,
+  HemisphereLight,
+  DirectionalLight,
+} from "three";
+  
 var camera;
 var renderer;
 var controls;
-var scene = new THREE.Scene();
+var scene = new Scene();
 var home;
+var helper_size;
 var building = {};
 
 /**
@@ -11,63 +23,32 @@ var building = {};
 function init() {
   var type = 'way';
   var id = 66418809;
-  var info = false;
   if (window.location.search.substr(1) !== null) {
     window.location.search.substr(1).split('&')
       .forEach(function(item) {
-        const tmp = item.split('=');
+        tmp = item.split('=');
         if (tmp[0] === 'type') {
           type = decodeURIComponent(tmp[1]);
         } else if (tmp[0] === 'id') {
           id = decodeURIComponent(tmp[1]);
-        } else if (tmp[0] === 'info') {
-          info = true;
         }
       });
   }
   Building.create(type, id).then(function(myObj){
-    const helperSize = myObj.outerElement.getWidth();
-    const helper = new THREE.GridHelper(helperSize / 0.9, helperSize / 9);
+    const helper_size = myObj.outer_element.getWidth();
+    const helper = new GridHelper(helper_size / 0.9, helper_size / 9);
     scene.add(helper);
 
     myObj.render();
-    if (info) {
-      const elem = document.createElement('div');
-      elem.setAttribute('id', 'div-building-details');
-      elem.setAttribute('style', 'position:absolute; top:10px; display: block; z-index: 100; background-color: #FFFFFF');
-      const target = document.querySelector('canvas');
-      target.before(elem);
-      const info = myObj.getInfo();
-      var partsString = '';
-      for (let i = 0; i < info.parts.length; i++) {
-        partsString += '<div class="building-part collapsible" style="border-style: solid"> <input type="checkbox" id="building ' + info.parts[i].id + '" onclick="ShowHideDiv(this)" /> <input type="checkbox" id="roof' + info.parts[i].id + '" onclick="ShowHideDiv(this)" /> <span>Type: ' + info.parts[i].type + '</span><span>ID: ' + info.parts[i].id + '</span></div><div class="content"><span>Options: ' + JSON.stringify(info.parts[i].options) + '</span></div>';
-      }
-      elem.innerHTML = '<div class="infobox"><div class="topBuilding"><span>Type: ' + info.type + '</span><span> ID: ' + info.id + '</span><span>Options: ' + JSON.stringify(info.options) + '</span></div>' + partsString + '</div>';
-      // Get building details from myObj
-      var coll = document.getElementsByClassName('collapsible');
-      var i;
-
-      for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener('click', function() {
-          this.classList.toggle('active');
-          var content = this.nextElementSibling;
-          if (content.style.display === 'block') {
-            content.style.display = 'none';
-          } else {
-            content.style.display = 'block';
-          }
-        });
-      }
-    }
   });
-  camera = new THREE.PerspectiveCamera(
+  camera = new PerspectiveCamera(
     50,
     document.documentElement.clientWidth /
       document.documentElement.clientHeight,
     0.1,
     1000,
   );
-  renderer = new THREE.WebGLRenderer({
+  renderer = new WebGLRenderer({
     alpha: false,
   });
   renderer.setSize(
@@ -86,10 +67,8 @@ function init() {
 function createScene() {
   addLights();
   camera.position.set(0, 0, 200); // x y z
-  camera.far = 50000;
-  camera.updateProjectionMatrix();
 
-  controls = new THREE.OrbitControls( camera, renderer.domElement );
+  controls =new OrbitControls( camera, renderer.domElement );
   function render() {
     requestAnimationFrame(render);
 
@@ -99,14 +78,14 @@ function createScene() {
 }
 
 function addLights() {
-  const ambientLight = new THREE.AmbientLight( 0xcccccc, 0.2 );
+  const ambientLight =new AmbientLight( 0xcccccc, 0.2 );
   scene.add( ambientLight );
 
-  var hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+  var hemiLight = new HemisphereLight( 0xffffff, 0xffffff, 0.6 );
   hemiLight.position.set( 0, 500, 0 );
   scene.add( hemiLight );
 
-  var dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
+  var dirLight = new DirectionalLight( 0xffffff, 1 );
   dirLight.position.set( -1, 0.75, 1 );
   dirLight.position.multiplyScalar( 1000 );
   scene.add( dirLight );
@@ -114,7 +93,6 @@ function addLights() {
 
 init();
 createScene();
-
 window.addEventListener('resize', resize, false);
 
 function resize() {
