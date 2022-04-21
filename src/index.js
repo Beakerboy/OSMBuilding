991778170d816948d7b610755677a681d6be4201
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-04-20 20:55:00 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -101,17 +101,17 @@ function addLights() {
 init();
 createScene();
 
-var coll = document.getElementsByClassName("collapsible");
+var coll = document.getElementsByClassName('collapsible');
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+  coll[i].addEventListener('click', function() {
+    this.classList.toggle('active');
     var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+    if (content.style.display === 'block') {
+      content.style.display = 'none';
     } else {
-      content.style.display = "block";
+      content.style.display = 'block';
     }
   });
 }
