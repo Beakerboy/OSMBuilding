f69b83acbb3d594ece80099389af73cb10db5ec3
Beakerboy1 <104165023+Beakerboy1@users.noreply.github.com>
2022-04-22 10:22:52 -0400

    Update buildingpart.js

--- src/buildingpart.js
+++ src/buildingpart.js
@@ -134,6 +134,7 @@ class BuildingPart {
       (calculatedOptions.roof.shape === 'flat' ? 0 : null) ??
       (calculatedOptions.roof.shape === 'dome' || calculatedOptions.roof.shape === 'pyramidal' ? BuildingShapeUtils.calulateRadius(this.shape) : null) ??
       (calculatedOptions.roof.shape === 'skillion' ? (calculatedOptions.roof.angle ? Math.cos(calculatedOptions.roof.angle / 360 * 2 * Math.PI) * BuildingShapeUtils.heightFacing(this.shape, calculatedOptions.roof.angle / 360 * 2 * Math.PI) : 22.5) : null);
+    console.log('roof height: ' + calculatedOptions.roof.height);
     calculatedOptions.building.height = this.options.specified.building.height ??
       this.options.inherited.building.height ??
       (calculatedOptions.building.levels * 3) + calculatedOptions.roof.height;
