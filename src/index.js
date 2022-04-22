b99d8126db7ec604d0396c6ba7cb7d67233b8bd6
Beakerboy1 <104165023+Beakerboy1@users.noreply.github.com>
2022-04-22 09:24:16 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ var building = {};
 function init() {
   var type = 'way';
   var id = 66418809;
+  var info = false;
   if (window.location.search.substr(1) !== null) {
     window.location.search.substr(1).split('&')
       .forEach(function(item) {
@@ -31,6 +32,8 @@ function init() {
           type = decodeURIComponent(tmp[1]);
         } else if (tmp[0] === 'id') {
           id = decodeURIComponent(tmp[1]);
+        } else if (tmp[0] === 'info') {
+          info = true;
         }
       });
   }
@@ -42,6 +45,34 @@ function init() {
     const mesh = myObj.render();
     for (let i = 0; i < mesh.length; i++) {
       scene.add(mesh[i]);
+    }
+    if (info) {
+      const elem = document.createElement('div');
+      elem.setAttribute('id', 'div-building-details');
+      elem.setAttribute('style', 'position:absolute; top:10px; display: block; z-index: 100; background-color: #FFFFFF');
+      const target = document.querySelector('canvas');
+      target.before(elem);
+      const info = myObj.getInfo();
+      var partsString = '';
+      for (let i = 0; i < info.parts.length; i++) {
+        partsString += '<div class="building-part collapsible" style="border-style: solid"> <input type="checkbox" id="building ' + info.parts[i].id + '" onclick="ShowHideDiv(this)" /> <input type="checkbox" id="roof' + info.parts[i].id + '" onclick="ShowHideDiv(this)" /> <span>Type: ' + info.parts[i].type + '</span><span>ID: ' + info.parts[i].id + '</span></div><div class="content"><span>Options: ' + JSON.stringify(info.parts[i].options) + '</span></div>';
+      }
+      elem.innerHTML = '<div class="infobox"><div class="topBuilding"><span>Type: ' + info.type + '</span><span> ID: ' + info.id + '</span><span>Options: ' + JSON.stringify(info.options) + '</span></div>' + partsString + '</div>';
+      // Get building details from myObj
+      var coll = document.getElementsByClassName('collapsible');
+      var i;
+
+      for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener('click', function() {
+          this.classList.toggle('active');
+          var content = this.nextElementSibling;
+          if (content.style.display === 'block') {
+            content.style.display = 'none';
+          } else {
+            content.style.display = 'block';
+          }
+        });
+      }
     }
   });
   camera = new PerspectiveCamera(
