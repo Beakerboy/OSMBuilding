0ec428cf0fa0f14f4496a9991baba40b0d00bb03
Beakerboy1 <104165023+Beakerboy1@users.noreply.github.com>
2022-05-03 08:48:18 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -63,7 +63,9 @@ function init() {
       for (let i = 0; i < info.parts.length; i++) {
         info.parts[i].options.inherited = {};
         info.parts[i].options.specified = {};
-        partsString += '<div class="building-part collapsible" style="border-style: solid"> <input type="checkbox" id="b' + info.parts[i].id + '" onclick="showHideDiv(\'b' + info.parts[i].id + '\')" /> <input type="checkbox" id="r' + info.parts[i].id + '" onclick="showHideDiv(\'r' + info.parts[i].id + '\')" /> <span>Type: ' + info.parts[i].type + '</span><span>ID: ' + info.parts[i].id + '</span></div><div class="content"><span>Options: ' + JSON.stringify(info.parts[i].options) + '</span></div>';
+        partsString += '<div class="building-part collapsible" style="border-style: solid"> <input type="checkbox" id="b' + info.parts[i].id + '" /> <input type="checkbox" id="r' + info.parts[i].id + '" /> <span>Type: ' + info.parts[i].type + '</span><span>ID: ' + info.parts[i].id + '</span></div><div class="content"><span>Options: ' + JSON.stringify(info.parts[i].options) + '</span></div>';
+        document.querySelector('b' + info.parts[i].id).addEventListener('click', showHideDiv('b' + info.parts[i].id));
+        document.querySelector('r' + info.parts[i].id).addEventListener('click', showHideDiv('r' + info.parts[i].id));
       }
       info.options.inherited = {};
       info.options.specified = {};
