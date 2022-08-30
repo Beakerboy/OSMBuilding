8ee03248861499930790b8a9875cf98d88e90611
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-08-30 08:12:44 -0400

    check if string

--- src/buildingpart.js
+++ src/buildingpart.js
@@ -332,23 +332,25 @@ class BuildingPart {
    * a float in meters.
    */
   static normalizeLength(length) {
-    if (length.includes('km')){
-      // remove final character.
-      return parseFloat(length.substring(0, length.length - 2)) * 1000;
+    if (typeof length ==='string' || length instanceof String) {
+      if (length.includes('km')){
+        // remove final character.
+        return parseFloat(length.substring(0, length.length - 2)) * 1000;
+      }
+     if (length.includes('mi')){
+        // remove final character.
+        return parseFloat(length.substring(0, length.length - 2)) * 5280 * 12 * 2.54 / 100;
+      }
+      if (length.includes('nmi')){
+        // remove final character.
+        return parseFloat(length.substring(0, length.length - 3)) * 1852;
+      }
+      // if feet and inches {
+      //   feet = parseFloat(feet_substr);
+      //   inches = parseFloat(inch_substr);
+      //   return (feet + inches / 12) * 0.3048;
+      // } else
     }
-    if (length.includes('mi')){
-      // remove final character.
-      return parseFloat(length.substring(0, length.length - 2)) * 5280 * 12 * 2.54 / 100;
-    }
-    if (length.includes('nmi')){
-      // remove final character.
-      return parseFloat(length.substring(0, length.length - 3)) * 1852;
-    }
-    // if feet and inches {
-    //   feet = parseFloat(feet_substr);
-    //   inches = parseFloat(inch_substr);
-    //   return (feet + inches / 12) * 0.3048;
-    // } else
     if (length) {
       return parseFloat(length);
     }
