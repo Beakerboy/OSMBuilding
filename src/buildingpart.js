903415339f3058dee37fa0f40fc38bd80addfa5f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-08-31 12:06:42 -0400

    Update buildingpart.js

--- src/buildingpart.js
+++ src/buildingpart.js
@@ -302,7 +302,7 @@ class BuildingPart {
   getAttribute(key) {
     if (this.way.querySelector('[k="' + key + '"]') !== null) {
       // if the buiilding part has a helght tag, use it.
-      window.printError('Element: ' + this.way.querySelector('[k="' + key + '"]'));
+      window.printError('Element: ' + this.way.querySelector('[k="' + key + '"]').html());
       window.printError('Attribute: ' + key + ' Value: ' + this.way.querySelector('[k="' + key + '"]').getAttribute('v'));
       
       return this.way.querySelector('[k="' + key + '"]').getAttribute('v');
