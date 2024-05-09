779cd56462afcbc28b22c7a231f86cd7657bcf0e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-05-09 14:37:07 -0400

    Update add.js

--- test/add.js
+++ test/add.js
@@ -1,8 +1,10 @@
-QUnit.test('Closed Way', assert => {
-  const jsdom = require("jsdom");
-  const { JSDOM } = jsdom
-  var way = '<way id="1"><nd ref="2"/><nd ref="3"/><nd ref="4"/><nd ref="5"/><nd ref="2"/></way>';
-  let parser = new JSDOM.window.DOMParser();
-  let xmlData = parser(way);
-  assert.true(BuildingShapeUtils.isClosed(xmlData));
+Qunit.module('qunit testing', function () {
+  QUnit.test('Closed Way', assert => {
+    const jsdom = require('jsdom');
+    const { JSDOM } = jsdom
+    var way = '<way id="1"><nd ref="2"/><nd ref="3"/><nd ref="4"/><nd ref="5"/><nd ref="2"/></way>';
+    let parser = new JSDOM.window.DOMParser();
+    let xmlData = parser(way);
+    assert.true(BuildingShapeUtils.isClosed(xmlData));
+  });
 });
