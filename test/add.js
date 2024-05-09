50cc6e9a65e61477ce1ebf0c2756e9939d1c967e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-05-09 14:18:07 -0400

    Update add.js

--- test/add.js
+++ test/add.js
@@ -1,5 +1,5 @@
 QUnit.test('Closed Way', assert => {
-  var way = '<way id="1"><nd ref="2"/><nd ref="3"/><nd ref="4"/><nd ref="5"/><nd ref="2"/></way>'
+  var way = '<way id="1"><nd ref="2"/><nd ref="3"/><nd ref="4"/><nd ref="5"/><nd ref="2"/></way>';
   let xmlData = new window.DOMParser().parseFromString(way, 'text/xml');
   assert.true(BuildingShapeUtils.isClosed(xmlData));
 });
