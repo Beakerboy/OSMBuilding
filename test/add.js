49d1518856fb9c4a19b72c3d459c958bd1c515c5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-05-09 14:38:19 -0400

    Update add.js

--- test/add.js
+++ test/add.js
@@ -1,7 +1,7 @@
-Qunit.module('qunit testing', function () {
+QUnit.module('qunit testing', function() {
   QUnit.test('Closed Way', assert => {
     const jsdom = require('jsdom');
-    const { JSDOM } = jsdom
+    const { JSDOM } = jsdom;
     var way = '<way id="1"><nd ref="2"/><nd ref="3"/><nd ref="4"/><nd ref="5"/><nd ref="2"/></way>';
     let parser = new JSDOM.window.DOMParser();
     let xmlData = parser(way);
