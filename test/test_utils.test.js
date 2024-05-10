ef12c003df120375823ab780a8737ebae27e7b17
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-05-10 09:35:32 -0400

    Update test_utils.test.js

--- test/test_utils.test.js
+++ test/test_utils.test.js
@@ -1,12 +1,14 @@
-import { TextEncoder } from 'node:util';
-global.TextEncoder = TextEncoder;
-
 /**
  * @jest-environment jsdom
  */
 
+import { TextEncoder } from 'node:util';
+global.TextEncoder = TextEncoder;
+
+
+
 import { BuildingShapeUtils } from '../src/extras/BuildingShapeUtils.js';
-import { JSDOM } from 'jsdom';
+// import { JSDOM } from 'jsdom';
 
 test('Test Closed Way', () => {
   var way = '<way id="1"><nd ref="2"/><nd ref="3"/><nd ref="4"/><nd ref="5"/><nd ref="2"/></way>';
