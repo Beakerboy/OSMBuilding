4e257b9f57dcdeaae9c42333a8d58c8823902adc
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-05-10 09:20:11 -0400

    Update test_utils.test.js

--- test/test_utils.test.js
+++ test/test_utils.test.js
@@ -5,7 +5,7 @@
 import { BuildingShapeUtils } from '../src/extras/BuildingShapeUtils.js';
 import { JSDOM } from 'jsdom';
 
-test('Test Closed Way', () => {
+test('Test Closed Way', async () => {
   var way = '<way id="1"><nd ref="2"/><nd ref="3"/><nd ref="4"/><nd ref="5"/><nd ref="2"/></way>';
   let parser = new window.DOMParser();
   let xmlData = parser(way);
