f552796ff96ffbec7758fb64d080cdd6504ea180
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-05-10 08:32:57 -0400

    Update test_utils.js

--- test/test_utils.js
+++ test/test_utils.js
@@ -1,13 +1,13 @@
 import { BuildingShapeUtils } from '../src/extras/BuildingShapeUtils.js';
 
-const jsdom = require('jsdom');
+// const jsdom = require('jsdom');
 
 QUnit.module('qunit testing', function() {
   QUnit.test('Closed Way', assert => {
-    const { JSDOM } = jsdom;
+    // const { JSDOM } = jsdom;
     var way = '<way id="1"><nd ref="2"/><nd ref="3"/><nd ref="4"/><nd ref="5"/><nd ref="2"/></way>';
-    let parser = new JSDOM.window.DOMParser();
-    // let parser = new window.DOMParser();
+    // let parser = new JSDOM.window.DOMParser();
+    let parser = new document.DOMParser();
     let xmlData = parser(way);
     assert.true(BuildingShapeUtils.isClosed(xmlData));
   });
