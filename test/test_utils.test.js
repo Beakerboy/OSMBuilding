084f51c67d89d30577a930a54af785017090b6e7
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-05-10 09:30:36 -0400

    Update test_utils.test.js

--- test/test_utils.test.js
+++ test/test_utils.test.js
@@ -10,7 +10,7 @@ import { JSDOM } from 'jsdom';
 
 test('Test Closed Way', () => {
   var way = '<way id="1"><nd ref="2"/><nd ref="3"/><nd ref="4"/><nd ref="5"/><nd ref="2"/></way>';
-  let parser = new window.DOMParser();
+  let parser = new JSDOM.DOMParser();
   let xmlData = parser(way);
   expect(BuildingShapeUtils.isClosed(xmlData)).toBe(true);
 });
