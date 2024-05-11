e8e9346881c8342a20dc147c6e7b60be22e9398b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-05-11 07:39:16 -0400

    Update test_utils.test.js

--- test/test_utils.test.js
+++ test/test_utils.test.js
@@ -23,3 +23,15 @@ test('Test Open Way', () => {
   let xmlData = parser.parseFromString(way, 'text/xml');
   expect(BuildingShapeUtils.isClosed(xmlData)).toBe(false);
 });
+
+test('Test combining 2 ways', () => {
+  var way1 = '<way id="1"><nd ref="1"/><nd ref="2"/><nd ref="3"/></way>';
+  var way2 = '<way id="2"><nd ref="3"/><nd ref="4"/><nd ref="1"/></way>';
+  var way3 = '<way id="1"><nd ref="1"/><nd ref="2"/><nd ref="3"/><nd ref="4"/><nd ref="1"/></way>
+  let parser = new window.DOMParser();
+  let xml1 = parser.parseFromString(way1, 'text/xml');
+  let xml2 = parser.parseFromString(way2, 'text/xml');
+  let result = BuildingShapeUtils.combineWays([way1, way2]);
+  let expected = parser.parseFromString(way3, 'text/xml');
+  expect(result.isEqualNode(expected)).toBe(true);
+});
