c822e36537c03de6b397e431120e7b3c4c7989ff
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-05-11 07:40:43 -0400

    Update test_utils.test.js

--- test/test_utils.test.js
+++ test/test_utils.test.js
@@ -27,7 +27,7 @@ test('Test Open Way', () => {
 test('Test combining 2 ways', () => {
   var way1 = '<way id="1"><nd ref="1"/><nd ref="2"/><nd ref="3"/></way>';
   var way2 = '<way id="2"><nd ref="3"/><nd ref="4"/><nd ref="1"/></way>';
-  var way3 = '<way id="1"><nd ref="1"/><nd ref="2"/><nd ref="3"/><nd ref="4"/><nd ref="1"/></way>
+  var way3 = '<way id="1"><nd ref="1"/><nd ref="2"/><nd ref="3"/><nd ref="4"/><nd ref="1"/></way>';
   let parser = new window.DOMParser();
   let xml1 = parser.parseFromString(way1, 'text/xml');
   let xml2 = parser.parseFromString(way2, 'text/xml');
