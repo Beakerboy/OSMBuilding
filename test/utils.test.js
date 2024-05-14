601e09c52a7069e716b5e65e4ebca1abe71abf66
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-05-14 10:42:10 -0400

    Update utils.test.js

--- test/utils.test.js
+++ test/utils.test.js
@@ -55,6 +55,6 @@ test('Extents no rotation', () => {
   shape.lineTo(-1, 1);
   expect(BuildingShapeUtils.extents(shape)).toStrictEqual([-1, -1, 1, 1]);
   const angle = 45 / 360 * 2 * 3.1415926535;
-  const sqrt2 = 1.41421356
-  expect(BuildingShapeUtils.extents(shape, angle)).toStrictEqual([-sqrt2, 0, sqrt2, sqrt2])
+  const sqrt2 = 1.41421356;
+  expect(BuildingShapeUtils.extents(shape, angle)).toStrictEqual([-sqrt2, 0, sqrt2, sqrt2]);
 });
