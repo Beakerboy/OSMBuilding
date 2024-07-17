b731728b7ee2de686b548b05b1a5297788c8c51d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-07-17 07:59:50 -0400

    Update buildingpart.test.js

--- test/buildingpart.test.js
+++ test/buildingpart.test.js
@@ -2,5 +2,6 @@ import { BuildingPart } from '../src/buildingpart.js';
 
 test('Test Cardinal to Degree', () => {
   expect(BuildingPart.cardinalToDegree('N')).toBe(0);
+  expect(BuildingPart.cardinalToDegree('sSw')).toBe(202);
   expect(BuildingPart.cardinalToDegree('Wse')).toBeUndefined();
 });
