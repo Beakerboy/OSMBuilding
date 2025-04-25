76d553a2abe0e5e4d44f4a41b53fa415b1100e53
Roman Deev <roman.deev06@gmail.com>
2025-04-25 15:15:57 +0300

    add osmApiUrl param for custom osm servers (#77)

--- src/apis.js
+++ src/apis.js
@@ -1,20 +1,21 @@
-let apis = {
+const osmApiUrl = new URLSearchParams(location.search).get('osmApiUrl') || 'https://api.openstreetmap.org/api/0.6';
+const apis = {
   bounding: {
-    api:'https://api.openstreetmap.org/api/0.6/map?bbox=',
+    api: osmApiUrl + '/map?bbox=',
     url: (left, bottom, right, top) => {
       return apis.bounding.api + left + ',' + bottom + ',' + right + ',' + top;
     },
   },
   getRelation: {
-    api:'https://api.openstreetmap.org/api/0.6/relation/',
-    parameters:'/full',
+    api: osmApiUrl + '/relation/',
+    parameters: '/full',
     url: (relationId) => {
       return apis.getRelation.api + relationId + apis.getRelation.parameters;
     },
   },
   getWay: {
-    api:'https://api.openstreetmap.org/api/0.6/way/',
-    parameters:'/full',
+    api: osmApiUrl + '/way/',
+    parameters: '/full',
     url: (wayId) => {
       return apis.getWay.api + wayId + apis.getWay.parameters;
     },
