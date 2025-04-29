661a8627c5451a0448fd4db36b42d936192a3e25
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-04-29 08:50:22 -0400

    Update utils.test.js

--- test/utils.test.js
+++ test/utils.test.js
@@ -81,10 +81,10 @@ describe('isSelfIntersecting', () => {
     ['<way id="1"><nd ref="1"/><nd ref="2"/></way>', false, 'open non-intersecting'],
     ['<way id="1"><nd ref="1"/><nd ref="2"/><nd ref="3"/><nd ref="1"/></way>', false, 'closed non-intersecting'],
     ['<way id="1"><nd ref="1"/><nd ref="2"/><nd ref="3"/><nd ref="2"/></way>', true, 'open intersecting'],
-    ['<way id="1"><nd ref="1"/><nd ref="2"/><nd ref="3"/><nd ref="4"/><nd ref="3"/><nd ref="1"/></way>', true, 'closed intersecting']
+    ['<way id="1"><nd ref="1"/><nd ref="2"/><nd ref="3"/><nd ref="4"/><nd ref="3"/><nd ref="1"/></way>', true, 'closed intersecting'],
     ])(${description}, (way, expected, description) => {
-    let parser = new window.DOMParser();
-    let xml = parser.parseFromString(way, 'text/xml').getElementsByTagName('way')[0];
-    expect(BuildingShapeUtils.isSelfIntersecting(xml)).toBe(expected);
+      let parser = new window.DOMParser();
+      let xml = parser.parseFromString(way, 'text/xml').getElementsByTagName('way')[0];
+      expect(BuildingShapeUtils.isSelfIntersecting(xml)).toBe(expected);
   });
 });
