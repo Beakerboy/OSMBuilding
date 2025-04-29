c946957d3fb15e7bf57fb38da3fa8a6d438e11dc
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-04-29 15:02:40 -0400

    Tested and debugged surrounds. (#97)

--- src/extras/BuildingShapeUtils.js
+++ src/extras/BuildingShapeUtils.js
@@ -344,11 +344,12 @@ class BuildingShapeUtils extends ShapeUtils {
   }
 
   /**
-   * Count the number of times that a line horizontal from point intersects shape
+   * Is the given point within the given shape?
    *
-   * if an odd number are crossed, it is inside.
-   * todo, test holes
-   * Test edge conditions.
+   * @param {THREE.Shape} shape - the shape
+   * @param {[number, number]} point - an x, y pair.
+   *
+   * @return {boolean}
    */
   static surrounds(shape, point) {
     var count = 0;
@@ -361,8 +362,13 @@ class BuildingShapeUtils extends ShapeUtils {
       if (vec.x === point[0] && vec.y === point[1]) {
         return true;
       }
-      if ((vec.x >= point[0] || nextvec.x >= point[0]) && (vec.y >= point[1] !== nextvec.y >= point[1])) {
+      const slope = (nextvec.y - vec.y) / (nextvec.x - vec.x);
+      const intercept = vec.y / slope / vec.x;
+      const intersection = (point[1] - intercept) / slope;
+      if (intersection > point[0]) {
         count++;
+      } else if (intersection === point[0]) {
+        return true;
       }
     }
     return count % 2 === 1;
--- test/utils.test.js
+++ test/utils.test.js
@@ -76,13 +76,13 @@ test('Longest side angle', () => {
   expect(BuildingShapeUtils.longestSideAngle(rightTriangle)).toBe(-Math.PI / 4);
 });
 
-describe('isSelfIntersecting', () => {
-  test.each([
-    ['<way id="1"><nd ref="1"/><nd ref="2"/></way>', false, 'open non-intersecting'],
-    ['<way id="1"><nd ref="1"/><nd ref="2"/><nd ref="3"/><nd ref="1"/></way>', false, 'closed non-intersecting'],
-    ['<way id="1"><nd ref="1"/><nd ref="2"/><nd ref="3"/><nd ref="2"/></way>', true, 'open intersecting'],
-    ['<way id="1"><nd ref="1"/><nd ref="2"/><nd ref="3"/><nd ref="4"/><nd ref="3"/><nd ref="1"/></way>', true, 'closed intersecting'],
-  ])('${description}', (way, expected, description) => {
+describe.each([
+  ['<way id="1"><nd ref="1"/><nd ref="2"/></way>', false, 'open non-intersecting'],
+  ['<way id="1"><nd ref="1"/><nd ref="2"/><nd ref="3"/><nd ref="1"/></way>', false, 'closed non-intersecting'],
+  ['<way id="1"><nd ref="1"/><nd ref="2"/><nd ref="3"/><nd ref="2"/></way>', true, 'open intersecting'],
+  ['<way id="1"><nd ref="1"/><nd ref="2"/><nd ref="3"/><nd ref="4"/><nd ref="3"/><nd ref="1"/></way>', true, 'closed intersecting'],
+])('isSelfIntersecting', (way, expected, description) => {
+  test(`${description}`, () => {
     let parser = new window.DOMParser();
     let xml = parser.parseFromString(way, 'text/xml').getElementsByTagName('way')[0];
     expect(BuildingShapeUtils.isSelfIntersecting(xml)).toBe(expected);
@@ -108,6 +108,16 @@ rightTriangle2.lineTo(1, -1);
 test('Vertex Angles counterclockwise', () => {
   expect(BuildingShapeUtils.vertexAngle(rightTriangle2)).toStrictEqual([-Math.PI / 2, -Math.PI / 4, -Math.PI / 4]);
 });
-// test surrounds
+
+describe.each([
+  [[-1, -1], false, 'Outside'],
+  [[1, 1], true, 'Share Node'],
+  [[.5, .5], true, 'Inside'],
+  [[0, 0], true, 'Border'],
+])('Surrounds', (point, expected, description) => {
+  test(`${description}`, () => {
+    expect(BuildingShapeUtils.surrounds(rightTriangle, point)).toBe(expected);
+  });
+});
 
 // test calcRadius
