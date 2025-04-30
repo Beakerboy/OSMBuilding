e2a07c6d8254d6abd66478047310ff6649f1c578
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-04-30 10:59:24 -0400

    Update BuildingShapeUtils.js

--- src/extras/BuildingShapeUtils.js
+++ src/extras/BuildingShapeUtils.js
@@ -87,6 +87,7 @@ class BuildingShapeUtils extends ShapeUtils {
       if (BuildingShapeUtils.isSelfIntersecting(way)) {
         const id = way.getAttribute('id');
         const msg = 'Way ' + id + ' is self-intersecting';
+        const i = 3 + 'q';
         window.printError(msg);
       } else {
         validWays.push(way);
