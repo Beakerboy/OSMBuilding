d5f4056212cdb67695fb49cd4c95172c1a09cf35
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-04-30 07:19:00 -0400

    Update combine_ways.test.js

--- test/combine_ways.test.js
+++ test/combine_ways.test.js
@@ -16,35 +16,35 @@ import { BuildingShapeUtils } from '../src/extras/BuildingShapeUtils.js';
 describe.each([
   [
     ['<way id="1"><nd ref="1"/><nd ref="2"/><nd ref="3"/><nd ref="4"/></way>',
-    ], 0, 0, 'Single Open Way',
+    ], 0, 0, [], 'Single Open Way',
   ],
   [
     [
       '<way id="1"><nd ref="1"/><nd ref="2"/></way>',
       '<way id="2"><nd ref="2"/><nd ref="3"/></way>',
       '<way id="3"><nd ref="3"/><nd ref="1"/></way>',
-    ], 1, 4, 'Test combining 3 ways 1->2->3',
+    ], 1, 4, [], 'Test combining 3 ways 1->2->3',
   ],
   [
     [
       '<way id="1"><nd ref="1"/><nd ref="2"/></way>',
       '<way id="2"><nd ref="3"/><nd ref="1"/></way>',
       '<way id="3"><nd ref="2"/><nd ref="3"/></way>',
-    ], 1, 4, 'Test combining 3 ways 2->1->3',
+    ], 1, 4, [], 'Test combining 3 ways 2->1->3',
   ],
   [
     [
       '<way id="1"><nd ref="1"/><nd ref="2"/></way>',
       '<way id="2"><nd ref="3"/><nd ref="2"/></way>',
       '<way id="3"><nd ref="3"/><nd ref="1"/></way>',
-    ], 1, 4, 'Test combining tip to tip',
+    ], 1, 4, [], 'Test combining tip to tip',
   ],
   [
     [
       '<way id="1"><nd ref="1"/><nd ref="2"/></way>',
       '<way id="2"><nd ref="1"/><nd ref="3"/></way>',
       '<way id="3"><nd ref="2"/><nd ref="3"/></way>',
-    ], 1, 4, 'Test combining tail to tail',
+    ], 1, 4, [], 'Test combining tail to tail',
   ],
   [
     [
@@ -52,20 +52,20 @@ describe.each([
       '<way id="2"><nd ref="3"/><nd ref="4"/></way>',
       '<way id="3"><nd ref="4"/><nd ref="1"/></way>',
       '<way id="4"><nd ref="2"/><nd ref="3"/></way>',
-    ], 1, 5, 'Test combining 4 ways',
+    ], 1, 5, [], 'Test combining 4 ways',
   ],
   [
     [
       '<way id="1"><nd ref="1"/><nd ref="2"/></way>',
       '<way id="2"><nd ref="3"/><nd ref="2"/></way>',
-    ], 0, 0, 'Test combining 2 open ways into one open way',
+    ], 0, 0, [], 'Test combining 2 open ways into one open way',
   ],
   [
     [
       '<way id="1"><nd ref="1"/><nd ref="2"/></way>',
       '<way id="2"><nd ref="3"/><nd ref="2"/></way>',
       '<way id="3"><nd ref="4"/><nd ref="5"/></way>',
-    ], 0, 0, 'Test combining 3 open ways into 2 open ways',
+    ], 0, 0, [], 'Test combining 3 open ways into 2 open ways',
   ],
   [
     [
@@ -73,28 +73,28 @@ describe.each([
       '<way id="2"><nd ref="2"/><nd ref="4"/></way>',
       '<way id="3"><nd ref="2"/><nd ref="3"/></way>',
       '<way id="4"><nd ref="1"/><nd ref="3"/></way>',
-    ], 1, 4, 'Combining 4 open ways into 1 closed & 1 remaining open way',
+    ], 1, 4, [], 'Combining 4 open ways into 1 closed & 1 remaining open way',
   ],
   [
     [
       '<way id="1"><nd ref="1"/><nd ref="2"/><nd ref="3"/></way>',
       '<way id="2"><nd ref="3"/><nd ref="5"/><nd ref="1"/></way>',
       '<way id="3"><nd ref="3"/><nd ref="4"/><nd ref="1"/></way>',
-    ], 1, 5, 'Dealing with amiguity. Only make one closed way',
+    ], 1, 5, [], 'Dealing with amiguity. Only make one closed way',
   ],
   [
     [
       '<way id="1"><nd ref="1"/><nd ref="2"/><nd ref="3"/></way>',
       '<way id="2"><nd ref="3"/><nd ref="4"/><nd ref="2"/><nd ref="5"/><nd ref="1"/></way>',
-    ], 0, 0, 'Closed way is self intersecting',
+    ], 0, 0, [], 'Closed way is self intersecting',
   ],
   [
     [
       '<way id="1"><nd ref="1"/><nd ref="2"/></way>',
       '<way id="2"><nd ref="2"/><nd ref="3"/><nd ref="4"/><nd ref="5"/><nd ref="3"/><nd ref="1"/></way>',
-    ], 0, 0, 'Open way is self intersecting',
+    ], 0, 0, ['Way 2 is self-intersecting'], 'Open way is self intersecting',
   ],
-])('Combine Ways', (ways, length, nodes, description) => {
+])('Combine Ways', (ways, length, nodes, errors, description) => {
   test(`${description}`, () => {
     let parser = new window.DOMParser();
     const xml = [];
