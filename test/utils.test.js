d3a12908b590ddc5c54765f4ab880f4d1d2150af
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-02 09:08:12 -0400

    Update utils.test.js

--- test/utils.test.js
+++ test/utils.test.js
@@ -25,10 +25,12 @@ test('', () => {
     3: [-1, 1],
   };
   const shape = BuildingShapeUtils.createShape(xmlData, nodelist);
-  expect(shape.extractPoints().shape.length).toBe(3);
+  expect(shape.extractPoints().shape.length).toBe(4);
   const points = shape.extractPoints().shape;
-  expect(points[1].x).toBe(1);
-  expect(points[1].y).toBe(1);
+  expect([points[1].x, points[1].y]).toBe(nodelist[1]);
+  expect([points[2].x, points[2].y]).toBe(nodelist[2]);
+  expect([points[3].x, points[3].y]).toBe(nodelist[3]);
+  expect([points[4].x, points[4].y]).toBe(nodelist[1]);
 });
 
 /** Test isClosed */
