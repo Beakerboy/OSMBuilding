2a998571466af8c0ff12782f576e5e691c10b68d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-06 11:52:44 -0400

    Update building.test.js

--- test/building.test.js
+++ test/building.test.js
@@ -90,8 +90,8 @@ test('Visible Outer Building', () => {
   const bldg = new Building('31361386', data);
   const mesh = bldg.render();
   //expect outer building and roof to be visible
-  expect(mesh[0].visible).toBe(false);
-  expect(mesh[1].visible).toBe(false);
+  expect(mesh[0].visible).toBe(true);
+  expect(mesh[1].visible).toBe(true);
 });
 
 window.printError = printError;
