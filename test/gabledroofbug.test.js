319b988c0f186a184535eea5f95befff2045c005
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-04 14:36:34 -0400

    Update gabledroofbug.test.js

--- test/gabledroofbug.test.js
+++ test/gabledroofbug.test.js
@@ -179,15 +179,15 @@ beforeEach(() => {
 
 // compare the object rendered from the requested way data to that rendered
 // from the requested map data to see if one errors and one does not.
-test('Constructor', () => {
-  const bldg = new Building('31361386', data);
-  expect(bldg.parts.length).toBe(1);
-  const part = bldg.parts[0];
-  const meshes = part.render();
-  const roofmesh = meshes[0];
-  const roofGeometry = roofmesh.geometry;
-  expect(roofGeometry.constructor.name).toBe('WedgeGeometry');
-});
+// test('Constructor', () => {
+//  const bldg = new Building('31361386', data);
+//  expect(bldg.parts.length).toBe(1);
+//  const part = bldg.parts[0];
+//  const meshes = part.render();
+//  const roofmesh = meshes[0];
+//  const roofGeometry = roofmesh.geometry;
+//  expect(roofGeometry.constructor.name).toBe('WedgeGeometry');
+// });
 
 window.printError = printError;
 
